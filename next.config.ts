6780d197efbe266223971794a75fbca569a74260
michaelgabrielwibowo <michaelgabrielwibowo@gmail.com>
2025-05-10 11:03:58 +0800

    push

--- next.config.ts
+++ next.config.ts
@@ -20,6 +20,10 @@ const nextConfig: NextConfig = {
   },
   webpack(config) {
     config.experiments = { ...config.experiments, asyncWebAssembly: true };
+    config.module.rules.push({
+      test: /\.wasm$/,
+      type: 'webassembly/async',
+    });
     return config;
   },
 };
