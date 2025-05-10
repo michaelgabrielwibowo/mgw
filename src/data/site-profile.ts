b2a81fad8bb31d1585fd540036196879ca86e32e
michaelgabrielwibowo <michaelgabrielwibowo@gmail.com>
2025-05-10 10:55:39 +0800

    push

---
+++ src/data/site-profile.ts
@@ -0,0 +1,9 @@
+// Only siteProfileData, no firebase-admin or Firestore imports
+export const siteProfileData = {
+  siteTitle: 'PersonaLink | Michael Gabriel Wibowo | Personal Page',
+  metaDescriptionName: 'Michael Gabriel Wibowo',
+  profileName: 'Michael Gabriel Wibowo',
+  tagline: 'International Student from Indonesia at Tunghai University, Taiwan',
+  profilePictureUrl: '/my image.jpg',
+  footerName: 'Michael Gabriel Wibowo',
+};
