4c7195d7b212699f001d5b879eede5a205136684
michaelgabrielwibowo <michaelgabrielwibowo@gmail.com>
2025-05-13 02:52:34 +0800

    delete

--- dataconnect-generated/js/default-connector/esm/index.esm.js
+++
@@ -1,7 +0,0 @@
-
-export const connectorConfig = {
-  connector: 'default',
-  service: 'studio',
-  location: 'asia-southeast1'
-};
-
--- dataconnect-generated/js/default-connector/index.cjs.js
+++
@@ -1,7 +0,0 @@
-
-const connectorConfig = {
-  connector: 'default',
-  service: 'studio',
-  location: 'asia-southeast1'
-};
-exports.connectorConfig = connectorConfig;
